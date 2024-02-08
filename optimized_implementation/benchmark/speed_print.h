314336b6d74c67bf6048623949c2a4289a5e8576
she-cryptolab <she000@cryptolab.co.kr>
2024-02-08 21:36:44 +0900

    Replace common files with symbolic link

--- optimized_implementation/benchmark/speed_print.h
+++ optimized_implementation/benchmark/speed_print.h
@@ -1,9 +1,1 @@
-#ifndef PRINT_SPEED_H
-#define PRINT_SPEED_H
-
-#include <stddef.h>
-#include <stdint.h>
-
-void print_results(const char *s, uint64_t *t, size_t tlen);
-
-#endif
+../../reference_implementation/benchmark/speed_print.h
