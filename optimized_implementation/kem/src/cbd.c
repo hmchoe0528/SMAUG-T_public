7b3b499fcb8ac6066f1f7e334f0c6e61107f705d
inkme <inkme@cryptolab.co.kr>
2024-09-01 23:33:10 +0900

    Update 90s and avx2

--- optimized_implementation/kem/src/cbd.c
+++ optimized_implementation/kem/src/cbd.c
@@ -34,18 +34,78 @@ static uint32_t load32_littleendian(const uint8_t x[4]) {
  *              - const uint8_t *buf: pointer to input byte array
  **************************************************/
 static void sp_cbd1(poly *r, const uint8_t buf[CBDSEED_BYTES]) {
-    unsigned int i, j;
-    uint8_t t, s;
-    int16_t d;
+    unsigned int i;
+    __m256i t, s, f0, f1, f2, f3;
+    const __m256i mask249 = _mm256_set1_epi32(0x249249);
+    const __m256i mask0FFF0000 = _mm256_set1_epi32(0x0FFF0000);
+    const __m256i mask00000FFF = _mm256_set1_epi32(0x00000FFF);
+    const __m256i mask3F00 = _mm256_set1_epi32(0x3F003F00);
+    const __m256i mask003F = _mm256_set1_epi32(0x003F003F);
+    const __m256i mask70 = _mm256_set1_epi32(0x70707070);
+    const __m256i mask07 = _mm256_set1_epi32(0x07070707);
+    const __m256i shufbidx = _mm256_set_epi8(
+        -1, 15, 14, 13, -1, 12, 11, 10, -1, 9, 8, 7, -1, 6, 5, 4, -1, 11, 10, 9,
+        -1, 8, 7, 6, -1, 5, 4, 3, -1, 2, 1, 0);
 
-    for (i = 0; i < LWE_N / 8; i++) {
-        t = buf[3 * i] & buf[3 * i + 1];
-        s = buf[3 * i + 2];
+    for (i = 0; i < CBDSEED_BYTES / 24; i++) {
+        t = _mm256_loadu_si256((__m256i *)&buf[24 * i]);
+        t = _mm256_permute4x64_epi64(t, 0x94);
+        t = _mm256_shuffle_epi8(t, shufbidx);
 
-        for (j = 0; j < 8; j++) {
-            d = (t >> j) & 0x01;
-            r->coeffs[8 * i + j] = d * ((((s >> j) << 1) & 0x02) - 1);
-        }
+        s = _mm256_and_si256(_mm256_srli_epi32(t, 2), mask249);
+        f0 = _mm256_and_si256(t, mask249);
+        f1 = _mm256_and_si256(_mm256_srli_epi32(t, 1), mask249);
+        t = _mm256_and_si256(f0, f1);
+
+        f0 = _mm256_and_si256(t, s);
+        f1 = _mm256_slli_epi32(f0, 1);
+        f0 = _mm256_or_si256(f0, f1);
+        f0 = _mm256_or_si256(f0, t);
+
+        f1 = _mm256_and_si256(_mm256_slli_epi32(f0, 4), mask0FFF0000);
+        f2 = _mm256_and_si256(f0, mask00000FFF);
+        f0 = _mm256_or_si256(f1, f2);
+
+        f1 = _mm256_and_si256(_mm256_slli_epi32(f0, 2), mask3F00);
+        f2 = _mm256_and_si256(f0, mask003F);
+        f0 = _mm256_or_si256(f1, f2);
+
+        f1 = _mm256_and_si256(_mm256_slli_epi32(f0, 1), mask70);
+        f2 = _mm256_and_si256(f0, mask07);
+        f0 = _mm256_or_si256(f1, f2);
+
+        f1 = _mm256_cvtepi8_epi16(_mm256_castsi256_si128(f0));
+        f2 = _mm256_and_si256(_mm256_srli_epi64(f1, 4), mask07);
+
+        f3 = _mm256_and_si256(f1, mask07);
+
+        __m256i temp_lo = _mm256_unpacklo_epi16(f3, f2);
+        __m256i temp_hi = _mm256_unpackhi_epi16(f3, f2);
+
+        f2 = _mm256_permute2x128_si256(temp_lo, temp_hi, 0x20);
+        f3 = _mm256_permute2x128_si256(temp_lo, temp_hi, 0x31);
+        f2 = _mm256_slli_epi16(f2, 14);
+        f2 = _mm256_srai_epi16(f2, 14);
+        f3 = _mm256_slli_epi16(f3, 14);
+        f3 = _mm256_srai_epi16(f3, 14);
+        _mm256_store_si256(&r->vec[4 * i], f2);
+        _mm256_store_si256(&r->vec[4 * i + 1], f3);
+
+        f0 = _mm256_cvtepi8_epi16(_mm256_extracti128_si256(f0, 1));
+        f2 = _mm256_and_si256(_mm256_srli_epi64(f0, 4), mask07);
+
+        f3 = _mm256_and_si256(f0, mask07);
+        temp_lo = _mm256_unpacklo_epi16(f3, f2);
+        temp_hi = _mm256_unpackhi_epi16(f3, f2);
+
+        f2 = _mm256_permute2x128_si256(temp_lo, temp_hi, 0x20);
+        f3 = _mm256_permute2x128_si256(temp_lo, temp_hi, 0x31);
+        f2 = _mm256_slli_epi16(f2, 14);
+        f2 = _mm256_srai_epi16(f2, 14);
+        f3 = _mm256_slli_epi16(f3, 14);
+        f3 = _mm256_srai_epi16(f3, 14);
+        _mm256_store_si256(&r->vec[4 * i + 2], f2);
+        _mm256_store_si256(&r->vec[4 * i + 3], f3);
     }
 }
 #endif
@@ -88,19 +148,56 @@ static void cbd(poly *r, const uint8_t buf[CBDSEED_BYTES]) {
  * Arguments:   - poly *r: pointer to output polynomial
  *              - const uint8_t *buf: pointer to input byte array
  **************************************************/
-static void sp_cbd2(poly *r, const uint8_t buf[CBDSEED_BYTES]) {
-    unsigned int i, j;
-    uint8_t t, s;
-    int16_t d;
+static void sp_cbd2(poly *r, const __m256i buf[CBDSEED_BYTES/32]) {
+    unsigned int i;
+    __m256i t, d, s, f0, f1, f2, f3;
+    const __m256i maskLSB = _mm256_set1_epi32(0x11111111);
+    const __m256i mask0F = _mm256_set1_epi32(0x000F000F);
+    __m256i lo, hi;
 
-    for (i = 0; i < LWE_N / 8; i++) {
-        t = buf[4 * i] & (buf[4 * i + 1] | buf[4 * i + 2]);
-        s = buf[4 * i + 3];
+    for(i = 0; i < CBDSEED_BYTES/32; i++) {
+        t = buf[i];
+        s = _mm256_and_si256(_mm256_srli_epi32(t, 3), maskLSB);
+        d = _mm256_and_si256(t, maskLSB);
+        d = _mm256_or_si256(d, _mm256_and_si256(_mm256_srli_epi32(t, 1), maskLSB));
+        d = _mm256_and_si256(d, _mm256_and_si256(_mm256_srli_epi32(t, 2), maskLSB));
 
-        for (j = 0; j < 8; j++) {
-            d = (t >> j) & 0x01;
-            r->coeffs[8 * i + j] = d * ((((s >> j) << 1) & 0x02) - 1);
-        }
+        f0 = _mm256_and_si256(d, s);
+        f1 = _mm256_slli_epi32(f0, 1);
+        f0 = _mm256_or_si256(f0, f1);
+        f0 = _mm256_or_si256(f0, d);
+
+        f1 = _mm256_cvtepi8_epi16(_mm256_castsi256_si128(f0));
+        f2 = _mm256_and_si256(_mm256_srli_epi64(f1, 4), mask0F);
+        f3 = _mm256_and_si256(f1, mask0F);
+
+        lo = _mm256_unpacklo_epi16(f3, f2);
+        hi = _mm256_unpackhi_epi16(f3, f2);
+
+        f2 = _mm256_permute2x128_si256(lo, hi, 0x20);
+        f3 = _mm256_permute2x128_si256(lo, hi, 0x31);
+        f2 = _mm256_slli_epi16(f2, 14);
+        f2 = _mm256_srai_epi16(f2, 14);
+        f3 = _mm256_slli_epi16(f3, 14);
+        f3 = _mm256_srai_epi16(f3, 14);
+        _mm256_store_si256(&r->vec[4 * i], f2);
+        _mm256_store_si256(&r->vec[4 * i + 1], f3);
+
+        f0 = _mm256_cvtepi8_epi16(_mm256_extracti128_si256(f0, 1));
+        f2 = _mm256_and_si256(_mm256_srli_epi64(f0, 4), mask0F);
+
+        f3 = _mm256_and_si256(f0, mask0F);
+        lo = _mm256_unpacklo_epi16(f3, f2);
+        hi = _mm256_unpackhi_epi16(f3, f2);
+
+        f2 = _mm256_permute2x128_si256(lo, hi, 0x20);
+        f3 = _mm256_permute2x128_si256(lo, hi, 0x31);
+        f2 = _mm256_slli_epi16(f2, 14);
+        f2 = _mm256_srai_epi16(f2, 14);
+        f3 = _mm256_slli_epi16(f3, 14);
+        f3 = _mm256_srai_epi16(f3, 14);
+        _mm256_store_si256(&r->vec[4 * i + 2], f2);
+        _mm256_store_si256(&r->vec[4 * i + 3], f3);
     }
 }
 #endif
@@ -111,6 +208,6 @@ void poly_cbd(poly *r, const uint8_t buf[CBDSEED_BYTES]) {
 #elif SMAUG_MODE == 3
     cbd(r, buf);
 #elif SMAUG_MODE == 5
-    sp_cbd2(r, buf);
+    sp_cbd2(r, (__m256i*)buf);
 #endif
 }
